00a649bdd449d06d88d6c5beb5a8248b21dc9bf7
kirankarkera <kiran.karkera@bridgei2i.com>
2015-02-27 17:38:34 +0530

    working html indent code

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject com.bi2i.extrack/srch-webapp "NOT_DEPLOYED"
-  :source-paths ["src/clj" ]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+(defproject clojure-examples "NOT_DEPLOYED"
+ :source-paths ["src/clj" ]
+ :dependencies [[org.clojure/clojure "1.6.0"]
                  [rmarianski/tidy-up "0.0.2"]
+                 [markdown-clj "0.9.63"]
                  [enlive "1.1.5"]
+                           [org.clojure/core.match "0.3.0-alpha4"]
+                           [zip-visit "1.0.2"]
+                 [gorilla-repl "0.3.5"]
                  [hiccup "1.0.5"]]
+ :plugins [[lein-gorilla "0.3.4"]]
 
-)
+ )
