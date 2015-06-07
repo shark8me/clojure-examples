e486ec64b8efc73d861f9e14c5754c49c7020146
kirankarkera <kiran.karkera@bridgei2i.com>
2015-06-07 15:38:09 +0530

    changed project.clj to use indent plugin

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
 (defproject clojure-examples "NOT_DEPLOYED"
- :source-paths ["src/clj" ]
+; :source-paths ["src/clj" ]
  :dependencies [[org.clojure/clojure "1.6.0"]
                  [rmarianski/tidy-up "0.0.2"]
                  [markdown-clj "0.9.63"]
                  [enlive "1.1.5"]
-                           [org.clojure/core.match "0.3.0-alpha4"]
-                           [zip-visit "1.0.2"]
-                 [gorilla-repl "0.3.5"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [zip-visit "1.0.2"]
+;                 [gorilla-repl "0.3.5"]
+                 [gorilla-indented-html-renderer "0.1.0-SNAPSHOT"]
                  [hiccup "1.0.5"]]
  :plugins [[lein-gorilla "0.3.4"]]
-
+ :target-path "target/%s"
+ :profiles {:uberjar {:aot :all}}
  )
